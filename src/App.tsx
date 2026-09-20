c7ca5fbc4004e0b3aa8e967c966b269f68753f8e
unknown <rahadshek22@gmail.com>
2026-09-21 04:54:35 +0600

    create nave bar on react

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import { Suspense } from "react";
-import Banner from "./Banner";
-import Nave from "./Nave";
-import Technologies from "./components/Technologies";
+// import { Suspense } from "react";
+// import Banner from "./Banner";
+// import Nave from "./Nave";
+// import Technologies from "./components/Technologies";
+// import { Http2ServerRequest } from "http2";
 
-function App() {
-  return (
-    <>
-      <Nave />
-      <Banner />
-      <Suspense
-        fallback={ 
-          <span className="loading loading-dots loading-xl"> </span>
-        }
-      >
-        <Technologies />
-      </Suspense>
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//       <Nave />
+//       <Banner />
+//       <Suspense
+//         fallback={ <h2>Loading</h2>
+//           <span className="loading loading-dots loading-xl"> </span>
+//         }
+//       >
+//         <Technologies />
+//       </Suspense>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
